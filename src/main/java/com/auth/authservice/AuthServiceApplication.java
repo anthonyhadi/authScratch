38fe438283e31572bb2da1265a3d62332e6a7097
c.anthonyh <anthony.hadi@emeriocorp.com>
2017-10-13 22:40:45 +0700

    update to the end of tutorial

--- src/main/java/com/auth/authservice/AuthServiceApplication.java
+++ src/main/java/com/auth/authservice/AuthServiceApplication.java
@@ -7,10 +7,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Entity;
@@ -24,15 +33,40 @@ public class AuthServiceApplication {
 
 	@Bean
 	CommandLineRunner demo(AccountRepository accountRepository) {
-		return args -> {
-			Stream.of("jlong.spring", "dsyer.cloud", "pwebb.boot", "rwinch.security")
+		return args ->
+			Stream.of("jlong,spring", "dsyer,cloud", "pwebb,boot", "rwinch,security")
 					.map( tpl -> tpl.split(","))
-					.forEach(tpl -> accountRepository.save(new Account()));
-		};
+					.forEach(tpl -> accountRepository.save(new Account(tpl[0], tpl[1], true)));
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+}
+
+@Configuration
+@EnableAuthorizationServer
+class AuthServiceConfiguration extends AuthorizationServerConfigurerAdapter {
+
+	private final AuthenticationManager authenticationManager;
+
+	public AuthServiceConfiguration(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+				.inMemory()
+				.withClient("html5")
+				.secret("password")
+				.authorizedGrantTypes("password")
+				.scopes("openid");
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(this.authenticationManager);
 	}
 }
 
@@ -47,7 +81,12 @@ class AccountUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		return accountRepository.findByUsername(username)
+				.map(account -> new User(account.getUsername(),
+						account.getPassword(), account.isActive(), account.isActive(), account.isActive(), account.isActive(),
+						AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER")
+				))
+				.orElseThrow(() -> new UsernameNotFoundException("Couldn't find the username " + username + "!"));
 	}
 }
 
@@ -62,8 +101,6 @@ interface AccountRepository extends JpaRepository<Account, Long> {
 @AllArgsConstructor
 @Entity
 class Account {
-	public Account() {
-	}
 
 	public Account(String username, String password, boolean active) {
 		this.username = username;
@@ -76,4 +113,36 @@ class Account {
 	private Long id;
 	private String username, password;
 	private boolean active;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
